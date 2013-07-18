b41aed663c064f86be2d51ed9d4f7e0fead3d7e0
Nick Bolten <nbolten@gmail.com>
2013-07-17 23:26:26 -0700

    Documentation for seqio module.

--- pymbt/seqio/__init__.py
+++ pymbt/seqio/__init__.py
@@ -1,8 +1,4 @@
-'''
-Read and write cloning-relevant formats (sequences, chromatograms, etc).
-
-'''
-
+'''Read and write cloning-relevant formats (sequences, chromatograms, etc).'''
 from pymbt.seqio._dna import read_dna
 from pymbt.seqio._dna import read_sequencing
 from pymbt.seqio._dna import write_dna
