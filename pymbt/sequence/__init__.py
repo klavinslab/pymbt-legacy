fb287778dc08a1c059b3b933d00bd7c79fbba97f
Nick Bolten <nbolten@gmail.com>
2013-10-29 18:43:30 -0700

    Removed bad import

--- pymbt/sequence/__init__.py
+++ pymbt/sequence/__init__.py
@@ -5,5 +5,4 @@ from pymbt.sequence._dna import Primer
 from pymbt.sequence._dna import Feature
 from pymbt.sequence._peptide import Peptide
 from pymbt.sequence._rna import RNA
-from pymbt.sequence import restriction_sites
 from pymbt.sequence import utils
